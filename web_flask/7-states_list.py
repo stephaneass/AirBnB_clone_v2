44ffaa34454437f93f5506f637d4046125141c4d
Stephane ASSOCLE <mensste30@gmail.com>
2023-07-26 01:07:37 +0000

    script that starts a Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
-
 app = Flask(__name__)
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
 @app.teardown_appcontext
-def tear_down(self):
-    """tear down app context"""
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/states_list', strict_slashes=False)
-def list_states():
-    """lists states from database
-    Returns:
-        HTML
-    """
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('7-states_list.html', all_states=all_states)
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
